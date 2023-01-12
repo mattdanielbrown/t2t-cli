d5ab8a4c7fb954720c74a1936ff0d51d27858647
Matt Daniel Brown <developer@mattdanielbrown.com>
2023-01-11 21:59:25 -0600

    Initial commit

---
+++ bin/cli.js
@@ -0,0 +1,43 @@
+const {Command} = require('commander');
+const {convertTemperature} = require('../lib/convert-temperature');
+const {parseArguments} = require('../lib/parse-arguments');
+const program = new Command();
+
+program
+		.name('t2t')
+		.description('CLI for converting temperatures between celsius and fahrenheit.')
+		.version('1.1.2')
+		.arguments('<temperatureAndScale>')
+		.option('-n, --no-scale', 'omit trailing letter indicating temperature scale from result')
+		.option('-d, --degree', 'include degree symbol in the result')
+		.action((temperatureAndScale) => {
+			
+			
+			let finalResult = '';
+			let degreeOrNotString = '';
+			let scaleOrNotString = '';
+			
+			const options = program.opts();
+			
+			const parsedInputs           = parseArguments(temperatureAndScale);
+			const parsedInputScale       = parsedInputs.scale;
+			const parsedInputTemperature = parsedInputs.temperature;
+			
+			const convertedTemperature   = convertTemperature(
+					parsedInputScale,
+					parsedInputTemperature
+			);
+			
+			const convertedScale = parsedInputs.scale === 'c' ? 'F' : 'C';
+			
+			degreeOrNotString = options.degree === true ? '°' : '';
+			scaleOrNotString = options.scale === false ? '' : convertedScale;
+			
+			finalResult = `${convertedTemperature}${degreeOrNotString}${scaleOrNotString}`;
+			
+			console.log(finalResult);
+			
+		});
+
+program.version('1.1.2', '-v, --version', 'output the current version');
+program.parse();
